b3f3358d3df61aabbc833a8b7f81bdf92133d765
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-12-22 09:43:45 -0300

    add mysql configuration

--- src/main/java/com/calebe/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/com/calebe/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package com.calebe.sdjpaintro.bootstrap;
 import com.calebe.sdjpaintro.domain.Book;
 import com.calebe.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -17,16 +19,12 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
-
-        System.out.println("Id: " + bookDDD.getId());
-
         Book saveDDD = bookRepository.save(bookDDD);
 
-        System.out.println("Id: " + saveDDD.getId());
-
         Book bookSIA = new Book("Spring In Action", "232325", "Oriely");
-
         Book saveSIA = bookRepository.save(bookSIA);
 
         bookRepository.findAll().forEach(book -> {
