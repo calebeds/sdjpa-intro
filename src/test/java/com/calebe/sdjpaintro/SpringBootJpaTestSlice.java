4535017a409ee7a0b7fb42d70277bf7d612fde47
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-12-22 08:20:31 -0300

    add jpa test slice example

---
+++ src/test/java/com/calebe/sdjpaintro/SpringBootJpaTestSlice.java
@@ -0,0 +1,27 @@
+package com.calebe.sdjpaintro;
+
+import com.calebe.sdjpaintro.domain.Book;
+import com.calebe.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+public class SpringBootJpaTestSlice {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJpaTestSlice() {
+        long countBefore = bookRepository.count();
+
+        bookRepository.save(new Book("My Book", "234324", "Self"));
+
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+}
